a0eafbc80f80b884d2b6b5b3aa6b1494c5a0eb60
ottoek <otto.e.kog@gmail.com>
2017-09-25 02:55:33 -0300

    initial commit

---
+++ app/src/main/java/inacap/otto/seguroapp/MainActivity.java
@@ -0,0 +1,128 @@
+package inacap.otto.seguroapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
+
+    public static final String ES_ASEGURABLE = "ES ASEGURABLE";
+    public static final String NO_ES_ASEGURABLE = "NO ES ASEGURABLE";
+    private EditText edtPatente;
+    //private EditText edtMarca;
+    private EditText edtModelo;
+    private EditText edtAnho;
+    private EditText edtValoruf;
+    private Button btnCalcular;
+    private Spinner spnMarca;
+    private ArrayAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtPatente = (EditText) findViewById(R.id.edt_patente);
+        //edtMarca = (EditText) findViewById(R.id.edt_marca);
+        edtModelo = (EditText) findViewById(R.id.edt_modelo);
+        edtAnho = (EditText) findViewById(R.id.edt_anho);
+        edtValoruf = (EditText) findViewById(R.id.edt_valoruf);
+        btnCalcular = (Button) findViewById(R.id.btn_calcular);
+        btnCalcular.setOnClickListener(this);
+        spnMarca = (Spinner) findViewById(R.id.edt_marca);
+        //adaptador para spinner
+        List<String> listMarcas = new ArrayList<>();
+        listMarcas.add("Ford");
+        listMarcas.add("Toyota");
+        listMarcas.add("Fiat");
+        listMarcas.add("Renault");
+        listMarcas.add("Chevrolet");
+        listMarcas.add("Otras");
+
+        adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, listMarcas);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnMarca.setAdapter(adapter);
+        spnMarca.setOnItemSelectedListener(this);
+
+        this.restaurarDatos();
+        setTitle("Seguro App - Asegurate!");
+    }
+
+    private void restaurarDatos() {
+        Bundle bundle = this.getIntent().getExtras();
+        if (bundle != null && !bundle.isEmpty()) {
+            Double rValorUF = bundle.getDouble("valoruf");
+            edtPatente.setText(bundle.getString("patente"));
+            //edtMarca.setText(bundle.getString("marca"));
+            spnMarca.setSelection(adapter.getPosition(bundle.getString("marca")));
+            edtModelo.setText(bundle.getString("modelo"));
+            edtAnho.setText(bundle.getString("anho"));
+            edtValoruf.setText("" + rValorUF);
+        }
+    }
+
+    public void calcularSeguro() {
+
+        if (!this.checkFields(edtPatente, edtModelo, edtAnho, edtValoruf)) {
+            return;
+        } else if (Integer.parseInt(edtAnho.getText().toString())> Calendar.getInstance().get(Calendar.YEAR)) {
+            Toast.makeText(this, "Año incorrecto", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        int antiguedad = Calendar.getInstance().get(Calendar.YEAR) - Integer.parseInt(edtAnho.getText().toString());
+        String validez = (antiguedad>10 ? NO_ES_ASEGURABLE : ES_ASEGURABLE);
+        double valorSeguro = 0d;
+        if(antiguedad<=10){
+            valorSeguro = 0.1* Double.parseDouble(edtValoruf.getText().toString()) *antiguedad;
+        }
+
+        Intent intent = new Intent(this, ResultActivity.class);
+        /* patente, marca, modelo, año, valor uf, asegurable, valor seguro)*/
+        intent.putExtra("patente", edtPatente.getText().toString());
+//        intent.putExtra("marca", edtMarca.getText().toString());
+        intent.putExtra("marca", spnMarca.getSelectedItem().toString());
+        intent.putExtra("modelo", edtModelo.getText().toString());
+        intent.putExtra("anho", edtAnho.getText().toString());
+        intent.putExtra("valoruf", Double.parseDouble(edtValoruf.getText().toString()));
+        intent.putExtra("valorSeguro", valorSeguro);
+        intent.putExtra("esAsegurable", validez);
+        startActivity(intent);
+    }
+
+    private boolean checkFields(EditText... args) {
+        for (EditText editText : args) {
+            if (editText.getText().toString().isEmpty()) {
+                Toast.makeText(this, editText.getHint() + " incorrecto", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        calcularSeguro();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
---
+++ app/src/main/java/inacap/otto/seguroapp/ResultActivity.java
@@ -0,0 +1,85 @@
+package inacap.otto.seguroapp;
+
+import android.content.Intent;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class ResultActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView txv_patente;
+    private TextView txv_valorseguro;
+    private TextView txv_esasegurable;
+    private TextView txv_marca;
+    private TextView txv_modelo;
+    private TextView txv_anho;
+    private TextView txv_valoruf;
+    private ImageView imv_resultado;
+    private Button btn_volver;
+
+    Bundle bundle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        bundle = this.getIntent().getExtras();
+        setTitle("Seguro App - Asegurate!");
+        txv_patente = (TextView) findViewById(R.id.txv_patente);
+        txv_marca = (TextView) findViewById(R.id.txv_marca);
+        txv_modelo = (TextView) findViewById(R.id.txv_modelo);
+        txv_anho = (TextView) findViewById(R.id.txv_anho);
+        txv_valoruf = (TextView) findViewById(R.id.txv_valoruf);
+        txv_esasegurable = (TextView) findViewById(R.id.txv_esasegurable);
+        txv_valorseguro = (TextView) findViewById(R.id.txv_valorseguro);
+        imv_resultado = (ImageView) findViewById(R.id.imv_resultado);
+        btn_volver = (Button) findViewById(R.id.btn_volver);
+
+        btn_volver.setOnClickListener(this);
+
+        String patente = bundle.getString("patente");
+        String marca = bundle.getString("marca");
+        String modelo = bundle.getString("modelo");
+        String anho = bundle.getString("anho");
+        Double valorUf = bundle.getDouble("valoruf");
+        Double valorSeguro = bundle.getDouble("valorSeguro");
+        String esAsegurable = bundle.getString("esAsegurable");
+
+        txv_patente.setText(patente);
+        txv_marca.setText(marca);
+        txv_modelo.setText(modelo);
+        txv_anho.setText(anho);
+        txv_valoruf.setText(valorUf.toString());
+        txv_valorseguro.setText(valorSeguro.toString());
+        txv_esasegurable.setText(esAsegurable);
+        if(esAsegurable.equals("ES ASEGURABLE")){
+            imv_resultado.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.asegurado));
+            Toast.makeText(getApplicationContext(), "ES ASEGURABLE", Toast.LENGTH_SHORT).show();
+        } else {
+            imv_resultado.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.img_noasegurado));
+            Toast.makeText(getApplicationContext(), "NO ES ASEGURABLE", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        volver(bundle);
+    }
+
+    private void volver(Bundle bundle) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("patente", bundle.getString("patente"));
+        intent.putExtra("marca", bundle.getString("marca"));
+        intent.putExtra("modelo", bundle.getString("modelo"));
+        intent.putExtra("anho", bundle.getString("anho"));
+        intent.putExtra("valoruf", bundle.getDouble("valoruf"));
+        startActivity(intent);
+    }
+}
